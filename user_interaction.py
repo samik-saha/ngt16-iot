9cf2b471f7240a24c8d94bbc0ee54e09fa51aebc
Samik Saha <samiksaha88@gmail.com>
2016-07-26 00:19:15 +0530

    Add files via upload

--- user_interaction.py
+++ user_interaction.py
@@ -1,32 +1,37 @@
 # This script handle all user interaction and display
-meeting_room_id=100
+meeting_room_id = 100
 
 from RPLCD import CharLCD
 import RPi.GPIO as GPIO
+import time
+import urllib, urllib2
 
-#Setup LCD pins
-lcd = CharLCD(pin_rs=26, pin_e=24, pins_data=[22, 18, 16, 12])
-
-# Setup Keypad pins 
 GPIO.setmode(GPIO.BOARD)
-MATRIX = [ [1,2,3,'A'],
-           [4,5,6,'B'],
-           [7,8,9,'C'],
-           ['*',0,'#','D'] ]
-ROW = [19,21,23,29]
-COL = [31,33,35,37]
+
+# Setup LCD pins
+lcd = CharLCD(pin_rs=26, pin_e=24, pins_data=[22, 18, 16, 12], numbering_mode=GPIO.BOARD)
+lcd.clear()
+
+# Setup Keypad pins
+MATRIX = [[1, 2, 3, 'A'],
+          [4, 5, 6, 'B'],
+          [7, 8, 9, 'C'],
+          ['*', 0, '#', 'D']]
+ROW = [19, 21, 23, 29]
+COL = [31, 33, 35, 37]
 
 for j in range(4):
-        GPIO.setup(COL[j], GPIO.OUT)
-        GPIO.output(COL[j], 1)
+    GPIO.setup(COL[j], GPIO.OUT)
+    GPIO.output(COL[j], 1)
 
 for i in range(4):
-        GPIO.setup(ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        
+    GPIO.setup(ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
 def read_keys(nKeys):
     n = 0
     input_value = ""
-    
+
     try:
         while (True):
             for j in range(4):
@@ -34,7 +39,7 @@ def read_keys(nKeys):
                 for i in range(4):
                     if GPIO.input(ROW[i]) == 0:
                         input_char = str(MATRIX[i][j])
-                        input_value=input_value + input_char
+                        input_value = input_value + input_char
                         n += 1
                         print input_char
                         lcd.write_string(input_char)
@@ -45,15 +50,34 @@ def read_keys(nKeys):
                 break
 
     except KeyboardInterrupt:
-            GPIO.cleanup()
+        GPIO.cleanup()
 
-    GPIO.cleanup()
     return input_value
 
 
-def activate_room():
-    lcd.write_string("Enter passcode: ")
-    passcode = read_keys(4)
-    print (passcode)
-    # check key with server
-    # if does not match try again
+url = 'http://runnerp11.codenvycorp.com:62744/meeting_room'
+data = ({
+    'meetingroom_id': '100'
+})
+
+
+def activate_room(passcode):
+    lcd.write_string('Enter passcode: ')
+    input_passcode = read_keys(4)
+    time.sleep(1)
+    lcd.clear()
+    lcd.write_string('Verifying... Please wait.')
+    time.sleep(1)
+    lcd.clear()
+    if passcode == input_passcode:
+        lcd.write_string('Meeting in Progress')
+        lcd.cursor_pos=(2,0)
+        lcd.write_string('For 10 min break press B')
+    else:
+        lcd.write_string ('Invalid passcode. Please try again')
+        return False
+
+    return True
+
+
+activate_room('1234')
